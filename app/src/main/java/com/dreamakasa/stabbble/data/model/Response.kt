3ad6d87ad019c06fbf7fe3f281f375ea3957e149
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2017-09-14 11:33:10 +0700

    Add endpoint to remote

--- app/src/main/java/com/dreamakasa/stabbble/data/model/Response.kt
+++ app/src/main/java/com/dreamakasa/stabbble/data/model/Response.kt
@@ -1,12 +1,57 @@
 package com.dreamakasa.stabbble.data.model
 
-class SampleRes(
-    var field: String
+class LinksRes(
+    var web: String,
+    var twitter: String
+)
+
+class UserRes(
+        var id: Int,
+        var name: String,
+        var username: String,
+        var html_url: String,
+        var avatar_url: String,
+        var bio: String,
+        var location: String,
+        var links: LinksRes,
+        var bucket_count: Int,
+        var comments_received_count: Int,
+        var followers_count: Int,
+        var followings_count: Int,
+        var likes_count: Int,
+        var likes_received_count: Int,
+        var projects_count: Int,
+        var rebounds_received_count: Int,
+        var shots_count: Int,
+        var teams_count: Int,
+        var can_upload_shot: Boolean,
+        var type: String,
+        var pro: Boolean,
+        var buckets_url: String,
+        var followers_url: String,
+        var following_url: String,
+        var likes_url: String,
+        var shots_url: String,
+        var teams_url: String,
+        var created_at: String,
+        var updated_at: String
+)
+
+class FollowerRes(
+        var id: Int,
+        var created_at: String,
+        var follower: UserRes
+)
+
+class FollowingRes(
+        var id: Int,
+        var created_at: String,
+        var followee: UserRes
 )
 
 class TokenRes(
-    var access_token: String,
-    var token_type: String,
-    var scope: String
+        var access_token: String,
+        var token_type: String,
+        var scope: String
 )
 
--- app/src/main/java/com/dreamakasa/stabbble/data/remote/StabbbleService.kt
+++ app/src/main/java/com/dreamakasa/stabbble/data/remote/StabbbleService.kt
@@ -1,11 +1,11 @@
 package com.dreamakasa.stabbble.data.remote
 
-import com.dreamakasa.stabbble.data.model.TokenRes
-import com.dreamakasa.stabbble.data.model.SampleReq
+import com.dreamakasa.stabbble.data.model.*
 import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 
 interface StabbbleService {
@@ -13,7 +13,12 @@ interface StabbbleService {
         val ENDPOINT = "https://api.dribbble.com/v1/"
     }
 
-    @GET("/token")
-    fun getAccessToken(): Observable<SampleReq>
+    @GET("/user")
+    fun getCurrentUser(): Observable<UserRes>
 
+    @GET("/user/followers")
+    fun getFollowers(@Query("page") page: Int, @Query("per_page") per_page: Int): List<FollowerRes>
+
+    @GET("/user/following")
+    fun getFollowing(@Query("page") page: Int, @Query("per_page") per_page: Int): List<FollowingRes>
 }
