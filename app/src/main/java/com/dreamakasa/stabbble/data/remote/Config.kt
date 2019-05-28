ff00df540382658ea6180c944fa52259f129434d
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2019-05-29 00:28:54 +0700

    Update Config.kt

--- app/src/main/java/com/dreamakasa/stabbble/data/remote/Config.kt
+++ app/src/main/java/com/dreamakasa/stabbble/data/remote/Config.kt
@@ -1,7 +1,7 @@
 package com.dreamakasa.stabbble.data.remote
 
 object Config{
-    val CLIENT_SECRET = "53844a00162fdc8bf865210798829253601fd1a4e2ce018715cc7b75145b1ceb"
-    val CLIENT_ID = "1c87d7c0ba298fbda20cf7f5eeefe350b8f1bf0a7102687f57162aebc2813d4e"
+    val CLIENT_SECRET = "PUT_YOUR_CLIENT_SECRET"
+    val CLIENT_ID = "PUT_YOUR_CLIENT_ID"
     val REDIRECT_URI = "stabbble://redirecturi"
 }
